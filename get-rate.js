d55809db4561de97981709b05519c1a7661a545b
mio <10892119+biuuu@users.noreply.github.com>
2020-06-11 16:20:41 +0800

    init

---
+++ get-rate.js
@@ -0,0 +1,44 @@
+const main = async () => {
+  let id10
+  let id1
+  let idSSR
+  await $.get('/gacha/list').then(data => {
+    
+    data.legend.lineup.forEach(item => {
+      if (item.times === '10') {
+        if (item.description.includes('SSレア')) {
+          if (item.campaign_gacha_ids && item.campaign_gacha_ids.length) {
+            let idx = Math.floor(Math.random() * item.campaign_gacha_ids.length)
+            idSSR = item.campaign_gacha_ids[idx].id
+          } else {
+            idSSR = item.id
+          }
+        } else {
+          id10 = item.id
+        }
+      } else if (item.times === '1') {
+        id1 = item.id
+      }
+    })
+  })
+  
+  let rate1 = ''
+  let rate10 = ''
+  let rateSSR = ''
+  if (id1) {
+    const res = await $.get(`/gacha/provision_ratio/${id1}/1`)
+    rate1 = res.appear
+  }
+  if (id10) {
+    const res = await $.get(`/gacha/provision_ratio/${id10}/1`)
+    rate10 = res.appear
+    
+  }
+  if (idSSR) {
+    const res = await $.get(`/gacha/provision_ratio/${idSSR}/1`)
+    rateSSR = res.appear
+  }
+  return [rate1, rate10, rateSSR]
+}
+
+module.exports = main
---
+++ index.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer')
+const rate = require('./get-rate')
+const fs = require('fs-extra')
+
+const waitClick = async (sel, page) => {
+  await page.waitForSelector(sel)
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(sel)
+  ])
+  console.log(`${sel} clicked`)
+}
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/84.0.4133.0')
+  await page.goto('http://game.granbluefantasy.jp/#tutorial/2/0')
+  console.log('loaded')
+  
+  await page.waitForSelector('.female')
+  await page.click('.female')
+
+  await waitClick('.btn-start', page)
+  
+  const [rate1, rate10, rateSSR] = await page.evaluate(rate)
+
+  await fs.ensureDir('./dist/')
+  await fs.outputJSON('./dist/normal.json', rate1)
+  await fs.outputJSON('./dist/sr.json', rate10)
+  await fs.outputJSON('./dist/ssr.json', rateSSR)
+  
+  await browser.close()
+})()
